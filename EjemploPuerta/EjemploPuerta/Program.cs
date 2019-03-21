9d31e0a6e46ef062e61664063355581a1b31b60f
ajvc1819 <antonio_jose9300@hotmail.com>
2019-03-21 15:39:13 +0100

    Primera version del programa puerta

---
+++ EjemploPuerta/EjemploPuerta/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPuerta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Puerta p = new Puerta();
+                Console.WriteLine("Valores iniciales...");
+                p.MostrarEstado();
+                Console.WriteLine();
+                Console.WriteLine("Vamos a abrir...");
+                p.Abrir();
+                p.SetAncho(80);
+                p.MostrarEstado();
+                Console.WriteLine();
+                Console.WriteLine("Ahora el portón...");
+                Porton p2 = new Porton();
+                p2.SetAncho(300);
+                p2.Bloquear();
+                p2.MostrarEstado();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ EjemploPuerta/EjemploPuerta/Puerta.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPuerta
+{
+    class Puerta
+    {
+       protected int ancho; 
+       protected int alto; 
+       protected int color; 
+       protected bool abierta;
+
+        public Puerta()
+        {
+            ancho = 50;
+            alto = 208;
+            color = 0xFFFFFF;
+            abierta = false;
+        }
+        public Puerta(int an, int al)
+        {
+            ancho = an;
+            alto = al;
+            color = 0xFFFFFF;
+            abierta = false;
+        }
+        public void Abrir()
+        {
+            abierta = true;
+        }
+        public void Cerrar()
+        {
+            abierta = false;
+        }
+        public int GetAncho()
+        {
+            return ancho;
+        }
+        public void SetAncho(int nuevoValor)
+        {
+            ancho = nuevoValor;
+        }
+        public void MostrarEstado()
+        {
+            Console.WriteLine("Ancho: {0}", ancho);
+            Console.WriteLine("Alto: {0}", alto);
+            Console.WriteLine("Color: {0}", color);
+            Console.WriteLine("Abierta: {0}", abierta);
+        }
+    }
+}
